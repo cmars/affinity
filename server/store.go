ace0893217f4afb16774f9b461001847b2225598
Casey Marshall <cmars@cmarstech.com>
2014-01-08 23:04:21 -0600

    Add role-based access control over resources to the storage interface.

--- server/store.go
+++ server/store.go
@@ -26,9 +26,41 @@ import (
 var NotFound error = fmt.Errorf("Not found")
 
 type Store interface {
-	AddGroup(group *Group) error
+	// CreateGroup creates a new group.
+	CreateGroup(group *Group) error
+	// GetGroup fetches a group by its unique id.
 	GetGroup(groupId string) (*Group, error)
+	// DeleteGroup deletes the group from storage by its unique id.
 	DeleteGroup(groupId string) error
+
+	// AddMember adds a user to an existing group.
 	AddMember(groupId string, user User) error
+	// DeleteMember removes a user from an existing group.
 	DeleteMember(groupId string, user User) error
+
+	// AddAdmin adds an administrative user to an existing group.
+	AddAdmin(groupId string, user User) error
+	// DeleteAdmin removes an administrative user from an existing group.
+	DeleteAdmin(groupId string, user User) error
+
+	// GetPermission fetches a permission by its unique id.
+	GetPermission(permId string) (*Permission, error)
+
+	// GetRole fetches a role by its unique id.
+	GetRole(roleId string) (*Role, error)
+
+	// AddPermission adds a permission to an existing role.
+	AddPermission(permId string, roleId string) error
+	// RemovePermission removes a permission from an existing role.
+	RemovePermission(permId string, roleId string) error
+
+	// GrantRole grants all the privileges of a given role to a user on a resource.
+	GrantRole(roleId string, user User, resourceUri string) error
+	// RevokeRole removes the privileges of a given role from a user on a resource.
+	RevokeRole(roleId string, user User, resourceUri string) error
+	// RevokeUser removes the privileges of a given role from a user for all resources.
+	RevokeUser(roleId string, user User) error
+
+	// GetPermissions gets the effective permissions granted to a user on a given resource.
+	GetPermissions(user User, resourceUri string) ([]*Permission, error)
 }
