92a985b7c3e1cbddb34b12f8e39efe43a8a96df0
Casey Marshall <cmars@cmarstech.com>
2014-02-18 10:17:21 -0600

    s/AGPL/LGPL/

--- suite_test.go
+++ suite_test.go
@@ -3,15 +3,15 @@
    Copyright (C) 2014  Canonical, Ltd.
 
    This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as published by
+   it under the terms of the GNU Library General Public License as published by
    the Free Software Foundation, version 3.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
+   GNU Library General Public License for more details.
 
-   You should have received a copy of the GNU Affero General Public License
+   You should have received a copy of the GNU Library General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
